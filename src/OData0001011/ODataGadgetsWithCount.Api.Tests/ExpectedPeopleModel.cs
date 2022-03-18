2b86548069eb2352b438eb30027a8a7214fcc977
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-03-18 14:43:02 +0530

    Added a new proejct to understand odata routing. -just formatting

--- src/OData0001011/ODataGadgetsWithCount.Api.Tests/ExpectedPeopleModel.cs
+++ src/OData0001011/ODataGadgetsWithCount.Api.Tests/ExpectedPeopleModel.cs
@@ -9,7 +9,6 @@ namespace OData0001010_Gadgets.Api.Tests
         public int ODataCount { get; set; } = default!;
         [JsonPropertyName("@odata.context")]
         public string ODataContext { get; set; } = default!;
-
         public List<ExpectedGadgetModel> value { get; set; } = default!;
     }
     public class ExpectedGadgetModel
